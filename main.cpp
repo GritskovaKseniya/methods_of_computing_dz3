a4da9c7136643553404a03d3885f3b8a719195c6
Kseniya <grickovaKS@gmail.com>
2020-05-18 14:00:45 +0300

    add functions

--- main.cpp
+++ main.cpp
@@ -3,21 +3,34 @@
 #include <fstream>
 using namespace std;
 
-main(){
+
+double createSelection (){
     double x, y, z;
     ofstream out;          // поток для записи
     out.open("D:\\sample3D.txt"); // окрываем файл для записи
     if (out.is_open()){
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 20; i++){
             x = rand() / double( RAND_MAX );
             y = rand() / double( RAND_MAX );
             z = rand() / double( RAND_MAX );
             double d = x*x + y*y + z*z;
             if(d < 1){
+                cout << " d" << i << " = " << d << endl;
+                cout << x*x << " " << y*y << " " << z*z << " " << endl;
                 out << x << " " << y << " " << z << " " << endl;
             } else i--; 
         }
     }
+
+}
+
+int createHistogramData (){
+    
+}
+
+main(){  
+    createSelection();
+
     std::cout << "End of program" << std::endl;
     return 0;
 }
